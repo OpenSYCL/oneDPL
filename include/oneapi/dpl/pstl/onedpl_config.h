d248a7fb89ff651bbe663aef6173c5d3bea5fae2
Aksel Alpay <aksel.alpay@uni-heidelberg.de>
2021-03-17 22:32:27 +0100

    Add group algorithm hipSYCL compatibility

--- include/oneapi/dpl/pstl/onedpl_config.h
+++ include/oneapi/dpl/pstl/onedpl_config.h
@@ -57,10 +57,140 @@ template <access_mode mode> struct mode_tag_t {
   explicit mode_tag_t() = default;
 };
 
+namespace ONEAPI {
+using namespace ::hipsycl::sycl;
+}
+
 namespace property {
 // oneDPL requires older noinit name, while hipSYCL provides no_init
 // as per SYCL 2020 final spec.
 using noinit = no_init;
+}
+
+// DPC++ pre-SYCL 2020 final group algorithm compatibility
+
+
+template <typename Group, typename Ptr, typename Predicate,
+          std::enable_if_t<is_group_v<std::decay_t<Group>>, bool> = true>
+bool any_of(Group g, Ptr first, Ptr last, Predicate pred) {
+  return joint_any_of(g, first, last, pred);
+}
+
+template <typename Group, typename Ptr, typename Predicate,
+          std::enable_if_t<is_group_v<std::decay_t<Group>>, bool> = true>
+bool all_of(Group g, Ptr first, Ptr last, Predicate pred) {
+  return joint_all_of(g, first, last, pred);
+}
+
+template <typename Group, typename Ptr, typename Predicate,
+          std::enable_if_t<is_group_v<std::decay_t<Group>>, bool> = true>
+bool none_of(Group g, Ptr first, Ptr last, Predicate pred) {
+  return joint_none_of(g, first, last, pred);
+}
+
+template <typename Group, typename Ptr, typename BinaryOperation,
+          std::enable_if_t<is_group_v<std::decay_t<Group>>, bool> = true>
+typename std::iterator_traits<Ptr>::value_type
+reduce(Group g, Ptr first, Ptr last, BinaryOperation binary_op) {
+  return joint_reduce(g, first, last, binary_op);
+}
+
+template <typename Group, typename Ptr, typename T, typename BinaryOperation,
+          std::enable_if_t<is_group_v<std::decay_t<Group>>, bool> = true>
+T reduce(Group g, Ptr first, Ptr last, T init, BinaryOperation binary_op) {
+  return joint_reduce(g, first, last, init, binary_op);
+}
+
+template <typename Group, typename InPtr, typename OutPtr,
+          typename BinaryOperation,
+          std::enable_if_t<is_group_v<std::decay_t<Group>>, bool> = true>
+OutPtr exclusive_scan(Group g, InPtr first, InPtr last, OutPtr result,
+                      BinaryOperation binary_op) {
+  return joint_exclusive_scan(g, first, last, result, binary_op);
+}
+
+template <typename Group, typename InPtr, typename OutPtr, typename T,
+          typename BinaryOperation,
+          std::enable_if_t<is_group_v<std::decay_t<Group>>, bool> = true>
+T exclusive_scan(Group g, InPtr first, InPtr last, OutPtr result, T init,
+                 BinaryOperation binary_op) {
+  return joint_exclusive_scan(g, first, last, result, init, binary_op);
+}
+
+template <typename Group, typename InPtr, typename OutPtr,
+          typename BinaryOperation,
+          std::enable_if_t<is_group_v<std::decay_t<Group>>, bool> = true>
+OutPtr inclusive_scan(Group g, InPtr first, InPtr last, OutPtr result,
+                      BinaryOperation binary_op) {
+  return joint_inclusive_scan(g, first, last, result, binary_op);
+}
+
+template <typename Group, typename InPtr, typename OutPtr, typename T,
+          typename BinaryOperation,
+          std::enable_if_t<is_group_v<std::decay_t<Group>>, bool> = true>
+T inclusive_scan(Group g, InPtr first, InPtr last, OutPtr result,
+                 BinaryOperation binary_op, T init) {
+  return joint_inclusive_scan(g, first, last, result, binary_op, init);
+}
+
+template <typename Group, typename T, typename Predicate>
+bool any_of(Group g, T x, Predicate pred) {
+  return group_any_of(g, x, pred);
+}
+
+template <typename Group>
+bool any_of(Group g, bool pred) {
+  return group_any_of(g, pred);
+}
+
+template <typename Group, typename T, typename Predicate>
+bool all_of(Group g, T x, Predicate pred) {
+  return group_all_of(g, x, pred);
+}
+
+template <typename Group>
+bool all_of(Group g, bool pred) {
+  return group_all_of(g, pred);
+}
+
+template <typename Group, typename T, typename Predicate>
+bool none_of(Group g, T x, Predicate pred) {
+  return group_none_of(g, x, pred);
+}
+
+template <typename Group>
+bool none_of(Group g, bool pred) {
+  return group_none_of(g, pred);
+}
+
+template <typename Group, typename T, typename BinaryOperation>
+T reduce(Group g, T x, BinaryOperation binary_op) {
+  return group_reduce(g, x, binary_op);
+}
+
+template <typename Group, typename V, typename T, typename BinaryOperation>
+T reduce(Group g, V x, T init, BinaryOperation binary_op){
+  return group_reduce(g, x, init, binary_op);
+}
+
+template <typename Group, typename T, typename BinaryOperation>
+T exclusive_scan(Group g, T x, BinaryOperation binary_op) {
+  return group_exclusive_scan(g, x, binary_op);
+}
+
+template <typename Group, typename V, typename T, typename BinaryOperation>
+T exclusive_scan(Group g, V x, T init, BinaryOperation binary_op) {
+  return group_exclusive_scan(g, x, init, binary_op);
+}
+
+template <typename Group, typename T, typename BinaryOperation>
+T inclusive_scan(Group g, T x, BinaryOperation binary_op) {
+  return group_inclusive_scan(g, x, binary_op);
+}
+
+template <typename Group, typename V, typename T, typename BinaryOperation>
+T inclusive_scan(Group g, V x, BinaryOperation binary_op, T init) {
+  return group_inclusive_scan(g, x, init, binary_op);
 }
 
 }
